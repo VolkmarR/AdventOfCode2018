2fac1eadc440c1dda801b9b19cb6dc3d152a6d61
DESKTOP-84QLTBO\volkm <volkmar@rigo.bz>
2018-12-09 09:49:08 +0100

    Start Solution Day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    class Program
+    {
+        private static List<string> LoadData()
+        {
+            return File.ReadLines("Input.txt").ToList();
+        }
+
+        private static string CalculatePart1(List<string> data)
+        {
+            return "";
+        }
+
+        private static string CalculatePart2(List<string> data)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            var data = LoadData();
+
+            Console.WriteLine($"Part1: {CalculatePart1(data)}");
+
+            Console.WriteLine($"Part2: {CalculatePart2(data)}");
+        }
+    }
+}
