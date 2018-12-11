9303d8d643854c2f836717cf310af54d03beddf8
DESKTOP-84QLTBO\volkm <volkmar@rigo.bz>
2018-12-11 23:43:02 +0100

    Optimized Solution of Day 11 so that Part 2 is calculated faster

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -48,44 +48,55 @@ namespace Day11
 
         static string CalculatePart1(int[,] data)
         {
-            var result = FindMaxPowerBox(data, 3);
+            var result = FindMaxPowerBox(data, 3, 3);
             return $"{result.X},{result.Y}";
         }
 
         static String CalculatePart2(int[,] data)
         {
-            var result = new Result { };
-            for (int size = 1; size <= data.GetLength(0); size++)
-            {
-                var sizeResult = FindMaxPowerBox(data, size);
-                if (sizeResult.Power > result.Power)
-                    result = sizeResult;
-            }
+            var result = FindMaxPowerBox(data, 1, 300);
             return $"{result.X},{result.Y},{result.Size}";
         }
 
-
-        private static Result FindMaxPowerBox(int[,] data, int size)
+        private static Result FindMaxPowerBox(int[,] data, int fromSize, int toSize)
         {
-            var result = new Result { Size = size };
-            for (int y = 1; y < data.GetLength(0) - size; y++)
-                for (int x = 1; x < data.GetLength(1) - size; x++)
-                {
-                    var power = 0;
-                    for (int yo = 0; yo < size; yo++)
-                        for (int xo = 0; xo < size; xo++)
-                            power += GetValue(data, x + xo, y + yo);
-
-                    if (power > result.Power)
+            var resultList = new List<Result>();
+            var buffer = new int[300, 300];
+            for (int size = fromSize; size <= toSize; size++)
+            {
+                var result = new Result { Size = size };
+                resultList.Add(result);
+                for (int y = 1; y < data.GetLength(0) - size; y++)
+                    for (int x = 1; x < data.GetLength(1) - size; x++)
                     {
-                        result.Power = power;
-                        result.X = x;
-                        result.Y = y;
+                        var power = 0;
+                        if (size == fromSize)
+                        {
+                            for (int yo = 0; yo < size; yo++)
+                                for (int xo = 0; xo < size; xo++)
+                                    power += GetValue(data, x + xo, y + yo);
+                        }
+                        else
+                        {
+                            // Optimization: Reuse the values of the last round and add the numbers of the right and bottom border
+                            power = GetValue(buffer, x, y);
+                            for (int yo = 0; yo < size; yo++)
+                                power += GetValue(data, x + size - 1, y + yo);
+                            for (int xo = 0; xo < size - 1; xo++)
+                                power += GetValue(data, x + xo, y + size - 1);
+                        }
+                        SetValue(buffer, x, y, power);
+
+                        if (power > result.Power)
+                        {
+                            result.Power = power;
+                            result.X = x;
+                            result.Y = y;
+                        }
+
                     }
-
-                }
-
-            return result;
+            }
+            return resultList.OrderByDescending(q => q.Power).First();
         }
 
         static void Main(string[] args)
