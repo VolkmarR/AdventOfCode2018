59a62cce6557b48e749fecf80347cc46196af38a
Volkmar Rigo <volkmar@rigo.bz>
2018-12-16 13:21:41 +0100

    Adden Solution for Day 16 (incomplete)

---
+++ Day16/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    class Program
+    {
+        class Sample
+        {
+            public int[] Before { get; set; }
+            public int[] After { get; set; }
+            public int[] Command { get; set; }
+
+            public Sample(string before, string command, string after)
+            {
+                before = before.Substring(9, before.Length - 10);
+                after = after.Substring(9, after.Length - 10);
+                Before = before.Split(',').Select(q => Convert.ToInt32(q.Trim())).ToArray();
+                After = after.Split(',').Select(q => Convert.ToInt32(q.Trim())).ToArray();
+                Command = command.Split(' ').Select(q => Convert.ToInt32(q.Trim())).ToArray();
+            }
+        }
+
+        static List<Sample> LoadData()
+        {
+            var result = new List<Sample>();
+            var data = File.ReadAllLines("input.txt");
+            var i = 0;
+            while (i < data.Length && data[i].StartsWith("Before"))
+            {
+                result.Add(new Sample(data[i], data[i + 1], data[i + 2]));
+                i += 4;
+            }
+
+            return result;
+        }
+
+        static int CalculatePart1(List<Sample> data)
+        {
+
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"part 1 {CalculatePart1(LoadData())}");
+
+        }
+    }
+}
---
+++ Day16/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Day16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day16")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("73cf24f6-f9dd-44b4-98fa-26e5ece3e374")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
