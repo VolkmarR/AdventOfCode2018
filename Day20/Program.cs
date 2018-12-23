f537747ce03d4382d4e2288ca90a4e74ebf784c9
Volkmar Rigo <volkmar@rigo.bz>
2018-12-23 09:26:17 +0100

    Tests for Day 20 ... works for Examples but not for input

---
+++ Day20/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    class Program
+    {
+        class Map
+        {
+            public char[,] Data = new char[1000, 1000];
+            public int StartX = 500;
+            public int StartY = 500;
+
+            public Map()
+            {
+                for (int y = 0; y < Data.GetLength(1); y++)
+                    for (int x = 0; x < Data.GetLength(0); x++)
+                        Data[x, y] = '#';
+
+                Data[StartX, StartY] = 'X';
+            }
+        }
+
+        static string LoadData()
+        {
+            return File.ReadLines("input.txt").First();
+        }
+        
+        static void BuildMap(string data, Map map, int count, int currentDataPos, int currentX, int currentY)
+        {
+            var x = currentX;
+            var y = currentY;
+            var pos = currentDataPos;
+            while (data[pos] != '$')
+            {
+                // Branch start
+                if (data[pos] == '(')
+                {
+                    pos++;
+                    BuildMap(data, map, count, pos, x, y);
+
+                    while (!(data[pos] == ')' && count == 0))
+                    {
+                        if (data[pos] == '(')
+                            count++;
+                        else if (data[pos] == ')')
+                            count--;
+
+                        pos++;
+                        if (count == 0 && data[pos - 1] == '|' && data[pos] != '|')
+                            BuildMap(data, map, count, pos, x, y);
+                    }
+                    return;
+                }
+                else if (data[pos] == '|' || data[pos] == ')')
+                {
+                    // jump to end
+                    while (!(data[pos] == ')' && count == 0))
+                    {
+                        if (data[pos] == '(')
+                            count++;
+                        else if (data[pos] == ')')
+                            count--;
+                        pos++;
+                    }
+                }
+                else
+                {
+                    // navigate trougth the map
+                    var item = data[pos];
+                    var dx = 0;
+                    var dy = 0;
+                    if (item == 'N')
+                        dy = -1;
+                    else if (item == 'S')
+                        dy = 1;
+                    else if (item == 'W')
+                        dx = -1;
+                    else if (item == 'E')
+                        dx = 1;
+
+                    x += dx;
+                    y += dy;
+                    map.Data[x, y] = dx != 0 ? '|' : '-';
+                    x += dx;
+                    y += dy;
+                    map.Data[x, y] = '.';
+                }
+                pos++;
+            }
+        }
+
+        static void Dump(char[,] map)
+        {
+            var sb = new StringBuilder();
+            for (var y = 0; y < map.GetLength(1); y++)
+            {
+                for (var x = 0; x < map.GetLength(0); x++)
+                    sb.Append(map[x, y]);
+                sb.AppendLine();
+            }
+
+            File.WriteAllText("out.txt", sb.ToString());
+            Console.WriteLine(sb.ToString());
+        }
+
+        static string GetRoomLocation(string route)
+        {
+            var x = 0;
+            var y = 0;
+            foreach (var item in route)
+            {
+                if (item == 'N')
+                    y--;
+                else if (item == 'S')
+                    y++;
+                else if (item == 'W')
+                    x--;
+                else if (item == 'E')
+                    x++;
+            }
+            return $"{x}/{y}";
+        }
+
+        static void Main(string[] args)
+        {
+            var data = LoadData();
+
+
+            // var routes = new List<string>();
+            // BuildRoute(data, routes);
+
+            var map = new Map();
+            BuildMap(data, map, 0, 1, map.StartX, map.StartY);
+
+            Dump(map.Data);
+        }
+    }
+
+}
---
+++ Day20/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Day20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day20")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f62f0baa-7429-4aa6-b21d-03cab2cb775e")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
